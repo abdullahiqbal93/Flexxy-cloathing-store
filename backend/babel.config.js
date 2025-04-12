555182b0d66cb19ae5d479bbe4a20312cfc93e91
abdullah <mohamedabdullah93@gmail.com>
2025-04-12 13:19:23 +0530

    babel fixes

---
+++ backend/babel.config.js
@@ -0,0 +1,24 @@
+export default {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+        modules: false,
+      },
+    ],
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
+};
