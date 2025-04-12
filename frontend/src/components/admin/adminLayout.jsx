356cea87b45d25513e584f1b509b6f931063e1d9
abdullah <mohamedabdullah93@gmail.com>
2025-04-12 12:58:35 +0530

    case mismatch fixed

--- frontend/src/components/admin/adminLayout.jsx
+++ frontend/src/components/admin/adminLayout.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import Sidebar from '@/components/admin/Sidebar';
+import Sidebar from '@/components/admin/sidebar';
 import { Menu } from 'lucide-react';
 
 const AdminLayout = () => {
