47c7a87e07b42005893472716da75b6376ad0ce6
abdullah <mohamedabdullah93@gmail.com>
2025-04-15 12:22:45 +0530

    updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,23 @@
 import { init } from "./lib/init.js";
 import { mainLogger } from "./lib/logger/winston.js";
 import { welcome } from "./lib/welcome.js";
+import { getServer } from "./lib/server.js";
 
 process.on("unhandledRejection", (...reason) => {
   mainLogger.error("Unhandled Rejection at:", reason);
 });
 
-init()
-  .then(() => {
-    mainLogger.info(welcome());
-  })
-  .catch((e) => {
-    mainLogger.error("Application start error", e);
-  });
+// Initialize the database connection when not in a serverless environment
+if (process.env.NODE_ENV !== 'production' || !process.env.VERCEL) {
+  init()
+    .then(() => {
+      mainLogger.info(welcome());
+    })
+    .catch((e) => {
+      mainLogger.error("Application start error", e);
+    });
+}
+
+// Export the Express app for serverless functions
+const app = getServer();
+export default app;
