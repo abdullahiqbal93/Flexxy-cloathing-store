9c7d33a58a55d24a5e199917c7df7a46d2b1a44e
abdullah <mohamedabdullah93@gmail.com>
2025-04-18 22:13:42 +0530

    changed

--- frontend/src/lib/axios/axios.js
+++ frontend/src/lib/axios/axios.js
@@ -27,7 +27,6 @@ axiosInstance.interceptors.response.use(
   async (error) => {
     if (error.response?.status === 401) {
       sessionStorage.removeItem("authToken"); 
-      window.location.href = "/login";
     }
     return Promise.reject(error);
   }
