665796ec5ddea1fd8d67198dd75e9bd772f09103
abdullah <mohamedabdullah93@gmail.com>
2025-04-18 22:28:31 +0530

    changes

--- backend/src/lib/server.js
+++ backend/src/lib/server.js
@@ -30,9 +30,10 @@ export const getServer = () => {
     origin: env.CLIENT_BASE_URL,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control'],
   }));
   
+  app.options('*', cors());
   app.use(express.json());
   app.use(cookieParser());
   app.use(helmet());
